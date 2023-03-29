f7fa928f203311fed09631e69834b8c7572ac91d
thiagocmarques <tmarques01@mynbcc.ca>
2023-03-29 11:37:36 -0300

    initial commit

---
+++ Server/public/app.js
@@ -0,0 +1,114 @@
+console.log('Ready');
+
+const q = selector => document.querySelector(selector);
+
+let gameStatus;
+let messages = [];
+
+const cells = document.querySelectorAll('.cell');
+const gameBoard = q("#game-board")
+const msgBoard = q("#message-board");
+const btnAction = q("#btnAction");
+const btnJoin = q("#btnJoin");
+const userDiv = q("#user");
+const frm = q("#frm");
+const txtUser = q("#txtUser");
+
+const socket = io();
+
+frm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // When user join the game
+  socket.emit('joinGame', JSON.stringify(
+    { playerName: txtUser.value.trim() }
+  ));
+});
+
+
+// Update the board with the current game state 
+function updateBoard(board) {
+  for (let i = 0; i < board.length; i++) {
+    cells[i].textContent = board[i]; 
+  } 
+};
+
+  // Displaying server messages 
+  socket.on('message', message => {
+    messages.push(message);
+    updateMessageBoard();
+  }); 
+
+  // Game restarted
+  socket.on('restartGame', message => {
+    messages = [];
+    messages.push(message);
+    updateMessageBoard();
+  }); 
+
+
+  // receiving the gameState broadcast from server
+  socket.on('updateState', state => {
+    const {gameState} = JSON.parse(state);
+    updateBoard(gameState.board);
+    updateUI(gameState);
+  }); 
+
+
+  // When the player clicks on a cell
+  cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+      if (cell.textContent === '') {
+        socket.emit('playerMove', cell.id);
+      }
+    });
+  });
+
+  // enable/disable UI elements according to game state
+  function updateUI(gameState) {
+    if (gameState.playerCount < 2){
+      gameStatus = 'waiting';
+      btnJoin.disabled = false;
+      btnAction.disabled = true;
+      txtUser.disabled = false;
+    }
+    else if (gameState.winner){
+      gameStatus = 'end';
+      btnJoin.disabled = true;
+      btnAction.disabled = false;
+      txtUser.disabled = true;
+    }
+    else{
+      gameStatus = 'playing';
+      btnJoin.disabled = true;
+      btnAction.disabled = false;
+      txtUser.disabled = true;
+    }
+  };
+
+  btnAction.addEventListener("click", doAction);
+
+  function doAction() {
+    switch (gameStatus){
+      case 'waiting':
+        break;
+      case 'playing':
+      case 'end':
+        socket.emit('resetGame');
+        break;
+    }
+  }
+
+  function updateMessageBoard(){
+    let lines = '';
+    for (let index = messages.length - 1; index >= 0; index--){
+      console.log(messages[index]);
+      if (index == messages.length - 1)
+        lines += `<p><div class="currentMsg">${messages[index]}</div></p>`;
+      else
+        lines += `<p><div class="oldMsg">${messages[index]}</div></p>`;
+    }
+    if (messages.length > 0)
+      msgBoard.innerHTML = lines;
+  }
+
---
+++ Server/src/server.js
@@ -0,0 +1,279 @@
+// New Brunwick Community College
+// Gaming Experience Development
+// Senior Year 2023 - Network for Programmers
+// Instructor: Chris Cusack
+// Students: Bruno Bortoli and Thiago Marques
+// Assignment 4
+
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 2000;
+const http = require('http');
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "../public")));
+
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    resetGame();
+});
+
+
+let players = [];
+let gameState = {
+  board: ['', '', '', '', '', '', '', '', ''],
+  winner: '',
+  currentPlayerId: null,
+  playerCount: 0
+};
+
+
+io.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected`);
+
+    // We will respond from the server and welcome the user
+    socket.emit('message', 'Welcome to GEX 2023 NFP Tic-Tac-Toe game');
+    
+    // Sending the gameState to whoever connected
+    socket.emit('updateState', JSON.stringify(
+      {gameState}
+    ));
+ 
+    // Player joins the game
+    socket.on('joinGame', (message) => {
+      const {playerName} = JSON.parse(message);
+      //console.log(`${playerName} id: ${socket.id}`);
+      gameState.playerCount = players.length;
+
+      let alreadyPlaying = checkAlreadyJoined(socket.id);
+      // if the player attemping to join is already joined
+      if (alreadyPlaying){
+        socket.emit('message', 'You already joined the game. Wait for the start');
+      } 
+      else if (gameState.playerCount < 2) {
+        // Assign symbol to player
+        let symbol = assignSymbol(); 
+        let player = newPlayer(socket.id, playerName, symbol);
+        gameState.playerCount++;
+
+        // Update all clients with player info
+        io.emit('message', `${player.name} joined the game with ${player.symbol} symbol`);
+        console.log(`User ${player.name} joined the game as ${player.symbol}`);
+
+        // Start game if there are 2 players after the last join
+        if (gameState.playerCount === 2) {
+          console.log('Game starting...');
+          //printPlayers();
+          startGame();
+        }
+
+      } // Reject player if game is already full
+      else if (gameState.playerCount === 2){
+        console.log(`User ${socket.id} tried to join the game but it is full`);
+        socket.emit('message', 'The game is full. Try again in few minutes');
+      }
+    }); 
+  
+    // Player makes a move
+    socket.on('playerMove', (index) => {
+      let player = findPlayerById(socket.id);
+      // console.log(`Index: ${index} - socketId: ${socket.id}`);
+      // console.log(`${player.name} clicked on ${index}`);
+      // console.log(`Current player = ${gameState.currentPlayerId}`);
+
+      if (gameState.winner){
+        socket.emit('message', 'The game is over. Start a new game to play.');
+      }
+      // Only current player is allowed to move
+      else if (socket.id === gameState.currentPlayerId) 
+      {
+        // Update board with move
+        gameState.board[index] = player.symbol;
+
+        // Check for winner or tie
+        let winner = checkWin(player);
+
+        if (winner) {
+          let finalMsg = `Game over! ${player.name} wins`;
+          console.log(finalMsg);
+          gameState.winner = player.name;
+          io.emit('message', finalMsg);
+
+        } // Check if it is a tie game
+        else if (!gameState.board.includes('')) {
+          let tieMsg = 'Game over! Tie game'; 
+          console.log(tieMsg);
+          gameState.winner = "tie";
+          io.emit('message', tieMsg);
+
+        } 
+        // if game not over, update the next player allowed to move
+        else {
+          gameState.currentPlayerId = findNextPlayerIdToMove();
+        }
+
+        // broadcasting the updated state
+        io.emit('updateState', JSON.stringify(
+          {gameState}
+        ));
+
+      }else{
+        if (findPlayerById(socket.id)!=null)
+          socket.emit('message', 'It is not you turn. Wait your opponent move.');
+        else
+          socket.emit('message', 'You are just watching. Movements are not allowed');
+
+      }
+    });
+  
+    // Player disconnects
+    socket.on('disconnect', () => {
+      console.log(`User ${socket.id} disconnected`);
+  
+      // Remove player from players object
+      if (checkAlreadyJoined(socket.id)){
+        let name = findPlayerById(socket.id).name;
+        players = players.filter(p=> p.id != socket.id);
+        gameState.playerCount--;
+        resetGame();
+        io.emit('updateState', JSON.stringify(
+          {gameState}
+        ));
+        io.emit('message', `${name} left the game.`);
+      }
+
+    });
+
+      // Player restarted the game
+      socket.on('resetGame', () => {
+        console.log(`Game restarted by ${socket.id} `);
+    
+        // Only users playing the game can restart it
+        if (checkAlreadyJoined(socket.id)){
+          let name = findPlayerById(socket.id).name;
+          startGame();
+          io.emit('restartGame', `${name} restarted the game.`);
+        }else{
+          socket.emit('message', 'You are just watching. Restarting the game is not allowed');
+        }
+  
+      });
+    
+  });
+  
+  // Starts the game
+  function startGame() {
+    console.log('Game started');
+    resetGame();
+    gameState.currentPlayerId = players[0].id;
+    //console.log(`${printGameState()}`);
+    let plrName = findPlayerById(gameState.currentPlayerId).name;
+    io.emit('message', `Game started. ${plrName} plays first.`)
+    io.emit('updateState', JSON.stringify(
+      {gameState}
+    ));
+     
+  }
+  
+  // puts the game in the initial state
+  function resetGame() {
+    gameState = {
+      board: ['', '', '', '', '', '', '', '', ''],
+      winner: '',
+      currentPlayerId: null,
+      playerCount: players.length
+    };
+    
+  }
+  
+  // check if one client has already joined the session
+  function checkAlreadyJoined(id){
+    let found = players.filter(p => p.id == id);
+    return found.length != 0;
+  }
+
+  // Check if a given player has won
+function checkWin(player) {
+    const symbol = player.symbol;
+    if (
+      checkSameSymbol(0, 1, 2, symbol)    // top row
+      || checkSameSymbol(3, 4, 5, symbol) // middle row
+      || checkSameSymbol(6, 7, 8, symbol) // bottom row
+      || checkSameSymbol(0, 3, 6, symbol) // left column
+      || checkSameSymbol(1, 4, 7, symbol) // middle column
+      || checkSameSymbol(2, 5, 8, symbol) // right column
+      || checkSameSymbol(0, 4, 8, symbol) // diagonal 1
+      || checkSameSymbol(2, 4, 6, symbol) // diagonal 2
+    ) {
+      return true;
+    }
+    return false;
+  };
+  
+  // check if the given cells have the same provided symbol
+  function checkSameSymbol(cell1, cell2, cell3, symbol){
+      let b = gameState.board;
+      return b[cell1] === symbol && b[cell2] === symbol && b[cell3] === symbol;
+  };
+
+  // search the players array for the player that has the given id
+  function findPlayerById(id){
+    for (const player of players){
+      if (player.id == id)
+        return player;
+    }
+    return null;
+
+  };
+
+  // helper function to log players array in details
+  function printPlayers(){
+    for (let i = 0 ; i < players.length ; i++){
+      let p = players[i];
+      console.log(`Player info:`);
+      console.log(`Id....:  ${p.id}`);
+      console.log(`Name..:  ${p.name}`);
+      console.log(`Symbol:  ${p.symbol}`);
+      console.log("---------------------");
+    }
+  }
+
+  // helper function to log the Game State object in details
+  function printGameState(){
+    console.log(`Game State info:`);
+    console.log(`Board..........:  ${gameState.board}`);
+    console.log(`CurrentPlayer..:  ${gameState.currentPlayerId}`);
+    console.log(`Winner.........:  ${gameState.winner}`);
+    console.log('----------------');
+    
+  }
+  
+  // add a new Player to the players array
+  const newPlayer = (id, name, symbol) => {
+    let newPlr = { id: id, name: name, symbol: symbol };
+    players.push(newPlr);
+    return newPlr;
+}
+
+// return the next player to move based on the current player
+function findNextPlayerIdToMove(){
+  if (gameState.currentPlayerId === players[0].id){
+    return players[1].id;
+  }else{
+    return players[0].id;
+  }
+}
+
+// return the symbol to assign to one player based on how many players are joined already
+function assignSymbol(){
+  if (players.length == 0){
+    return 'X';
+  }else{
+    return players[0].symbol === 'X' ? 'O' : 'X';
+  }
+}
